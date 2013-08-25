8b56806009413829b64c8e2f569a4dc36768055b
Evgeny Grablyk <evgeny.grablyk@gmail.com>
2013-08-25 22:34:49 +0300

    Preparations for merge

--- CMakeFiles/camera-simulator_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "camera-simulator_automoc.cpp"
-  "CMakeFiles/camera-simulator_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/camera-simulator_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
